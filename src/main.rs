7eed882b0f091615dfef96d2ab4dd0cbba6e3628
ruironghuang <ruirong.huang@sjtu.edu.cn>
2023-03-05 18:22:26 -0500

    query

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ use env_logger::Builder;
 use log::{error, info};
 
 fn main() {
+    Builder::new().parse_filters("debug").init();
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
-    println!("Hello, world!");
+    dbg!(&args);
+    let query = &args[1];
+    let file = &args[2];
+    info!("Hello, world!");
 }
