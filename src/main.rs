976343596c9c37af9c3df8647f2b87571fb25afe
ruironghuang <ruirong.huang@sjtu.edu.cn>
2023-03-05 18:37:07 -0500

    struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
-use std::env;
 use env_logger::Builder;
 use log::{error, info};
+use std::{env, fs};
 
 fn main() {
     Builder::new().parse_filters("debug").init();
+
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
-    let query = &args[1];
-    let file = &args[2];
-    info!("Hello, world!");
+    let config = parse_args(&args);
+
+    info!("searching for {} in file {}", config.query, config.file);
+
+    let content = fs::read_to_string(config.file).expect("read file fail");
+}
+
+struct Config {
+    query: String,
+    file: String,
+}
+
+fn parse_args(args: &Vec<String>) -> Config {
+    Config {
+        query: args[1].to_string(),
+        file: args[2].to_string(),
+    }
 }
