d82f0e4cba777469ca78fac07b69f1b92580fbf2
ruironghuang <ruirong.huang@sjtu.edu.cn>
2023-03-05 19:07:47 -0500

    err

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use env_logger::Builder;
 use log::{error, info};
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     Builder::new().parse_filters("debug").init();
 
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        error!("parse args: {}", err);
+        process::exit(1);
+    });
 
     info!("searching for {} in file {}", config.query, config.file);
 
@@ -19,11 +22,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
-        Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        Ok(Config {
             query: args[1].clone(),
             file: args[2].to_string(),
-        }
+        })
     }
 }
-
