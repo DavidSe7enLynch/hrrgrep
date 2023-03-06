f0a08c7f99d47dded427ec8cc7aff5caa6d05276
ruironghuang <ruirong.huang@sjtu.edu.cn>
2023-03-05 20:02:14 -0500

    tdd

---
+++ src/lib.rs
@@ -0,0 +1,47 @@
+use std::error::Error;
+use std::fs;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.file)?;
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub file: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        Ok(Config {
+            query: args[1].clone(),
+            file: args[2].to_string(),
+        })
+    }
+}
+
+fn search<'a> (query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "32";
+        let contents = "\
+abcd
+ef
+ few32 qw
+g";
+        println!("{}", contents);
+        assert_eq!(vec![" few32 qw"], search(query, contents));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,23 @@
 use env_logger::Builder;
 use log::{error, info};
-use std::error::Error;
-use std::{env, fs, process};
+use std::{env, process};
+use hrrgrep::Config;
 
 fn main() {
     Builder::new().parse_filters("debug").init();
 
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        error!("parse args: {}", err);
+    let config = Config::new(&args).unwrap_or_else(|e| {
+        error!("parse args: {e}");
         process::exit(1);
     });
 
     info!("searching for {} in file {}", config.query, config.file);
 
-    run(config);
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(config.file).expect("read file fail");
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        Ok(Config {
-            query: args[1].clone(),
-            file: args[2].to_string(),
-        })
+    if let Err(e) = hrrgrep::run(config) {
+        error!("run: {e}");
+        process::exit(1);
     }
 }
+
+
